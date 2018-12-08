c336abebcf39123131df519b9a926ae60bd5cad2
Pip Dunne <pipjdunne@gmail.com>
2018-12-08 13:26:48 +0000

    Module aliasing for static imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react';
+import logo from '#/logo.svg';
+import '#/App.css';
 
 class App extends Component {
   render() {
@@ -16,8 +16,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
